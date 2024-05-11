d31b0a76ef5b09986f7ac00cf2dd9cf2203ab5ba
yuanbopang <yuanbopang@gmail.com>
2024-05-10 20:49:54 -0700

    A

--- DACLUB/5.10/A_MovieFestival.cpp
+++ DACLUB/5.10/A_MovieFestival.cpp
@@ -1,57 +1,39 @@
-//
-// Created by 14653 on 24-5-10.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-#include "A_MovieFestival.h"
-#include<iostream>
-#include<algorithm>
-#include<array>
-#include<vector>
-using namespace std;
-struct tim{
+struct Time {
     int start;
     int end;
-    int count;
 };
-bool cmp(struct tim& a, struct tim & b){
-    return a.start<b.start;
+
+// 比较函数，根据结束时间排序
+bool cmp(const Time& a, const Time& b) {
+    return a.end < b.end;
 }
 
 int main() {
     int n;
-    cin >> n;
-    vector<struct tim> a(n);
+    std::cin >> n;
+    std::vector<Time> a(n);
 
     for (int i = 0; i < n; i++) {
-        cin >> a[i].start >> a[i].end;
-        a[i].count = 0;
+        std::cin >> a[i].start >> a[i].end;
     }
 
-    // 使用 std::sort 来排序时间
-    sort(a.begin(), a.end(), cmp);
+    // 根据结束时间排序
+    std::sort(a.begin(), a.end(), cmp);
 
-    // 初始化第一个电影的计数为 1
-    a[0].count = 1;
-
-    // 计算最大的电影观看序列
-    for (int i = 1; i < n; i++) {
-        int max_count = 0;
-        for (int j = 0; j < i; j++) {
-            if (a[i].start >= a[j].end && a[j].count > max_count) {
-                max_count = a[j].count;
-            }
-        }
-        a[i].count = max_count + 1;
-    }
-
-    // 寻找计数的最大值
-    int max_count = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i].count > max_count) {
-            max_count = a[i].count;
+    // 选择不重叠的时间段
+    int count = 0;
+    int last_end_time = -1;
+    for (const auto& movie : a) {
+        if (movie.start >= last_end_time) {
+            last_end_time = movie.end;
+            count++;
         }
     }
 
-    cout << max_count;
+    std::cout << count;
     return 0;
 }
---
+++ DACLUB/5.10/B - Sum of Two Values.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 14653 on 24-5-10.
+//
+#include<iostream>
+#include<algorithm>
+#include<array>
+#include<vector>
+using namespace std;
