8103485a90d1dbdd1210c04911e01424a28f5e4d
PangYuanbo <yuanbopang@outlook.com>
2024-06-01 14:50:47 -0700

    change

--- 0x12 队列/Learning/循环数组.cpp
+++ 0x12 队列/Learning/循环数组.cpp
@@ -1,3 +1,31 @@
 //
 // Created by AaronPang on 6/1/24.
 //
+#include<iostream>
+#define MAXN 5
+using namespace std;
+int queue[MAXN];
+int front=0,rear=0;
+bool empty(){
+    if(front==rear){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool full(){
+    if((rear+1)%MAXN==front){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+void addqueue(int value){
+    rear=(rear+1)%MAXN;
+    queue[rear]=value;
+}
+int main(){
+
+}
